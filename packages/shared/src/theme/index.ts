6cf7d19409f2224bdaa537e420c3714b7fe11ded
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-22 22:53:39 +0900

    feat: add typography

---
+++ packages/shared/src/theme/index.ts
@@ -0,0 +1,28 @@
+type Typestyle =
+  | 'headline1'
+  | 'headline2'
+  | 'headline3'
+  | 'title1'
+  | 'title2'
+  | 'body1'
+  | 'body2'
+  | 'caption1'
+  | 'caption2'
+
+export type Theme = {
+  [key in Typestyle]: string
+}
+
+const theme: Theme = {
+  headline1: 'font-weight: bold; font-size: 2.5rem; line-height: 3rem;',
+  headline2: 'font-weight: bold; font-size: 2rem; line-height: 2.375rem;',
+  headline3: 'font-weight: bold; font-size: 1.75rem; line-height: 2.125rem;',
+  title1: 'font-weight: bold; font-size: 1.25rem; line-height: 1.5rem;',
+  title2: 'font-weight: bold; font-size: 1.06rem; line-height: 1.3125rem;',
+  body1: 'font-weight: normal; font-size: 0.94rem; line-height: 1.3125rem;',
+  body2: 'font-weight: normal; font-size: 0.9rem; line-height: 1.25rem;',
+  caption1: 'font-weight: normal; font-size: 0.8rem; line-height: 1.2rem;',
+  caption2: 'font-weight: normal; font-size: 0.75rem; line-height: 1.06rem;',
+}
+
+export default theme
