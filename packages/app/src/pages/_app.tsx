300e87c21b1af17dc0501d6e2313f086e595fe7a
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-24 23:20:24 +0900

    [#15] font 설정 (#16)

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { typography } from 'shared'
 import { ThemeProvider } from '@emotion/react'
 import 'shared/dist/index.css'
+import '@/styles/font.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
