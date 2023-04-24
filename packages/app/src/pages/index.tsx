e854e9a480e80590f0cce707025aa1a2c5a2e38d
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-24 22:29:06 +0900

    chore: remove unnecessary code

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,9 +1,3 @@
-import styled from '@emotion/styled'
-
-const Main = styled.main`
-  ${(props) => props.theme.headline1}
-`
-
 export default function Home() {
-  return <Main style={{ background: 'var(--P1)' }}>Hello world</Main>
+  return <main>Hello world</main>
 }
