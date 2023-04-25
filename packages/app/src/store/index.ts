700ffde205665b473a77d6d11f3ce970ed707398
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-25 21:01:21 +0900

    feat: add redux-toolkit setting

---
+++ packages/app/src/store/index.ts
@@ -0,0 +1,29 @@
+import { AnyAction, combineReducers, configureStore } from '@reduxjs/toolkit'
+import { HYDRATE, createWrapper } from 'next-redux-wrapper'
+
+const reducers = combineReducers({})
+
+export type RootState = ReturnType<typeof reducers>
+
+const rootReducer = (state: RootState | undefined, action: AnyAction) => {
+  switch (action.type) {
+    case HYDRATE:
+      return { ...state, ...action.payload }
+    default:
+      return reducers(state, action)
+  }
+}
+
+const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    devTools: true,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ serializableCheck: false }),
+  })
+}
+
+export const store = makeStore()
+
+const wrapper = createWrapper(makeStore, { debug: false })
+export default wrapper
