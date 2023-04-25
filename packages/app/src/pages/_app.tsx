9e5df54e2a221d2e552cbbb6a20fde786b767a6f
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-25 21:02:25 +0900

    [#18] redux toolkit 설정 (#19)

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -2,12 +2,18 @@ import { typography } from 'shared'
 import { ThemeProvider } from '@emotion/react'
 import 'shared/dist/index.css'
 import '@/styles/font.css'
+import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
+import wrapper from '@/store'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, ...rest }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest)
+
   return (
     <ThemeProvider theme={typography}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...props.pageProps} />
+      </Provider>
     </ThemeProvider>
   )
 }
