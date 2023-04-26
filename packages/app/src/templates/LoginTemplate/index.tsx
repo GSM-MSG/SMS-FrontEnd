4c1041b9c9797a11ed6fc0316fb64a10325077ca
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-26 17:26:42 +0900

    feat: implement login page

--- packages/app/src/templates/LoginTemplate/index.tsx
+++ packages/app/src/templates/LoginTemplate/index.tsx
@@ -7,9 +7,25 @@ const LoginTemplate = () => {
 
   return (
     <S.Wrapper>
-      <Headline type='headline1'>{'STUDENT\nMANAGEMENT SERVICE'}</Headline>
-      <Title type='title2'>학생 정보 통합관리 서비스</Title>
-      <GauthLoginButton onClick={onClick} />
+      <S.Video muted autoPlay loop>
+        <source
+          src='/video/login-background.mp4'
+          type='video/mp4; codecs=hvc1'
+        />
+        <source
+          src='/video/login-background.webm'
+          type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"'
+        />
+      </S.Video>
+      <S.Text1>
+        <Headline type='headline1'>{'STUDENT\nMANAGEMENT SERVICE'}</Headline>
+      </S.Text1>
+      <S.Text2>
+        <Title type='title2'>학생 정보 통합관리 서비스</Title>
+      </S.Text2>
+      <S.Text3>
+        <GauthLoginButton onClick={onClick} />
+      </S.Text3>
     </S.Wrapper>
   )
 }
--- packages/app/src/templates/LoginTemplate/style.ts
+++ packages/app/src/templates/LoginTemplate/style.ts
@@ -1,6 +1,55 @@
+import { keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
 
 export const Wrapper = styled.main`
+  margin-top: 8rem;
   width: 100%;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1:nth-of-type(1) {
+    margin-bottom: 2rem;
+  }
+
+  h2:nth-of-type(1) {
+    margin-bottom: 3rem;
+  }
+`
+
+export const Video = styled.video`
+  width: 100%;
+  min-height: 100%;
+  position: absolute;
+  top: 0;
+  height: 0;
+  object-fit: cover;
+  z-index: -1;
+`
+
+const opacity = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(3rem);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`
+
+export const Text1 = styled.div`
+  animation-name: ${opacity};
+  animation-duration: 1s;
+`
+
+export const Text2 = styled.div`
+  animation-name: ${opacity};
+  animation-duration: 1.3s;
+`
+
+export const Text3 = styled.div`
+  animation-name: ${opacity};
+  animation-duration: 1.6s;
 `
