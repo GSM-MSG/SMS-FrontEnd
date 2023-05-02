490df801171373096ccf8415f60a8246915a4b8c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-05-02 10:57:39 +0900

    feat(button): button styling

---
+++ packages/shared/src/atoms/Button/index.tsx
@@ -0,0 +1,68 @@
+import styled from '@emotion/styled'
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react'
+import { ButtonTheme } from './type'
+
+interface Props
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  theme?: ButtonTheme
+}
+
+const Button = ({ theme = 'default', ...props }: Props) => {
+  switch (theme) {
+    case 'default':
+      return <Wrapper {...props} />
+    case 'white':
+      return <WhiteButton {...props} />
+  }
+}
+
+const Wrapper = styled.button`
+  border-radius: 0.5rem;
+  padding: 0.75rem;
+  outline: none;
+  cursor: pointer;
+  min-width: 12.5rem;
+  outline: none;
+  transition: 0.4s;
+  border: none;
+  ${(props) => props.theme.title2}
+  background: var(--P2);
+  color: var(--WHITE);
+
+  &:hover {
+    background: var(--P1);
+  }
+
+  &:focus {
+    background: var(--P3);
+  }
+
+  &:disabled {
+    background: var(--N20);
+    color: var(--N30);
+  }
+`
+
+const WhiteButton = styled.button`
+  border-radius: 0.5rem;
+  padding: 0.75rem;
+  outline: none;
+  cursor: pointer;
+  min-width: 12.5rem;
+  outline: none;
+  transition: 0.4s;
+  border: 1px solid var(--N20);
+  background: var(--WHITE);
+  color: var(--BLACK);
+  ${(props) => props.theme.title2}
+
+  &:hover {
+    border: 1px solid var(--N30);
+    background: var(--N10);
+  }
+`
+
+export default Button
