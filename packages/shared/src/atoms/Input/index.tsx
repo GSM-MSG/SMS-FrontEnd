08349e91d9177fa7a513eed15e2bf07477185cf8
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-05-03 17:05:10 +0900

    fix(Input): fix input reset feature

--- packages/shared/src/atoms/Input/index.tsx
+++ packages/shared/src/atoms/Input/index.tsx
@@ -1,6 +1,8 @@
 import {
+  ChangeEvent,
   DetailedHTMLProps,
   InputHTMLAttributes,
+  MouseEvent,
   forwardRef,
   useRef,
   useState,
@@ -18,32 +20,39 @@ interface Props
 
 const Input = forwardRef<HTMLInputElement, Props>(
   ({ error, ...props }, ref) => {
+    const [value, setValue] = useState<string>('')
     const [isFocused, setIsFocused] = useState<boolean>(false)
     const inputRef = useRef<HTMLInputElement>(null)
-
-    const onClick = () => {
-      if (ref && 'current' in ref && ref.current?.value) {
-        ref.current?.focus()
-        ref.current.value = ''
-      }
-
-      inputRef.current?.focus()
-      if (inputRef.current?.value) inputRef.current.value = ''
-    }
 
     const onFocus = () => setIsFocused(true)
     const onBlur = () => setIsFocused(false)
 
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value)
+      if (props.onChange) props.onChange(e)
+    }
+
+    const onReset = () => {
+      if (props.value) return (props.value = '')
+      setValue('')
+    }
+
     return (
       <S.Wrapper>
-        <S.InputWrapper isFocused={isFocused} onClick={onClick}>
+        <S.InputWrapper isFocused={isFocused}>
           <S.TextFiled
+            value={props.value ? props.value : value}
+            onChange={onChange}
             onFocus={onFocus}
             onBlur={onBlur}
             ref={ref ? ref : inputRef}
             {...props}
           />
-          <S.ResetButton type='reset'>
+          <S.ResetButton
+            style={{ opacity: props.value || value ? 1 : 0 }}
+            type='reset'
+            onClick={onReset}
+          >
             <img src={SmallXmark} />
           </S.ResetButton>
         </S.InputWrapper>
