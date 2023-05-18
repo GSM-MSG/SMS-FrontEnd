c5e40adf03436cccb5fef16180472dcf09aa949c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-05-19 01:15:49 +0900

    fix: fix file upload logic on FileInput

--- packages/shared/src/atoms/FileInput/style.ts
+++ packages/shared/src/atoms/FileInput/style.ts
@@ -1,5 +1,9 @@
 import styled from '@emotion/styled'
 
+export const TextInput = styled.input`
+  display: block;
+`
+
 export const Wrapper = styled.label`
   min-width: 12.5rem;
   padding: 0.8rem 0.75rem;
