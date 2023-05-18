873d04aabb802d9dcf0075ef967deacbf45473de
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-05-18 17:19:37 +0900

    feat: add DeleteButton component

---
+++ packages/shared/src/atoms/DeleteButton/index.tsx
@@ -0,0 +1,12 @@
+import { Trash } from '../../icons'
+import * as S from './style'
+
+const DeleteButton = () => {
+  return (
+    <S.Wrapper>
+      <Trash />
+    </S.Wrapper>
+  )
+}
+
+export default DeleteButton
