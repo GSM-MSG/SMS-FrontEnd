7d2f1b6e7f4bb6a34f08c23af72a592f8d242983
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-05-18 19:21:36 +0900

    feat: add LanguageInputs components

---
+++ packages/app/src/features/register/molecules/LanguageInputs/index.tsx
@@ -0,0 +1,27 @@
+import { MultiDoubleInput } from '@sms/shared'
+import { InputColumn, FormWrapper } from '@features/register/atoms'
+import { Control, FieldValues, UseFormRegister } from 'react-hook-form'
+
+interface Props<FormType extends FieldValues> {
+  register: UseFormRegister<FormType>
+  control: Control<FormType>
+}
+
+const LanguageInputs = <FormType extends FieldValues>({
+  control,
+  register,
+}: Props<FormType>) => {
+  return (
+    <FormWrapper title='외국어'>
+      <InputColumn comment='외국어'>
+        <MultiDoubleInput
+          register={register}
+          control={control}
+          name='languageCertificate'
+        />
+      </InputColumn>
+    </FormWrapper>
+  )
+}
+
+export default LanguageInputs
