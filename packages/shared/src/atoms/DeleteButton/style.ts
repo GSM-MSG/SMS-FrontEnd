94235d154b788c44d27796357bedab454a119252
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-05-18 17:28:21 +0900

    [#57] 삭제 버튼 추가 (#58)

---
+++ packages/shared/src/atoms/DeleteButton/style.ts
@@ -0,0 +1,43 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.button`
+  width: 2.5rem;
+  height: 2.5rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  position: relative;
+  z-index: 100;
+
+  path {
+    transition: 0.1s;
+  }
+
+  ::before {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 100%;
+    border-radius: 0.5rem;
+    background: var(--N10);
+    transform: scale(0);
+    transition: 0.1s;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+
+  :hover {
+    ::before {
+      transform: scale(1);
+    }
+
+    path:not(:nth-of-type(2)) {
+      fill: var(--ERROR);
+    }
+    path:nth-of-type(2) {
+      fill: var(--WHITE);
+    }
+  }
+`
