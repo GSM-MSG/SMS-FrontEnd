c413cd6fb8c63c744b4c3c59bf7a48a543b3e369
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-05-18 11:05:58 +0900

    feat: add MultiDoubleInput component

---
+++ packages/shared/src/molecules/MultiDoubleInput/index.tsx
@@ -0,0 +1,57 @@
+import { useEffect } from 'react'
+import {
+  Control,
+  FieldValues,
+  UseFormRegister,
+  useFieldArray,
+} from 'react-hook-form'
+import { Trash } from '../../icons'
+import { Chip, Input } from '../../atoms'
+import * as S from './style'
+
+interface Props {
+  name: string
+  control: Control<any>
+  register: UseFormRegister<any>
+}
+
+const MultiDoubleInput = ({ name, control, register }: Props) => {
+  const { fields, append, remove } = useFieldArray<FieldValues>({
+    name,
+    control,
+  })
+
+  useEffect(() => {
+    append({})
+  }, [])
+
+  return (
+    <S.Wrapper>
+      <S.InputsWrapper>
+        {fields.map((field, index) => (
+          <S.InputWrapper key={field.id}>
+            <S.DoubleInputWrapper>
+              <Input
+                {...register(`${name}[${index}].languageCertificateName`)}
+                placeholder='예) 토익'
+              />
+              <Input
+                {...register(`${name}[${index}].score`)}
+                placeholder='990'
+              />
+            </S.DoubleInputWrapper>
+
+            {fields.length > 1 && (
+              <S.RemoveBtn type='button' onClick={() => remove(index)}>
+                <Trash />
+              </S.RemoveBtn>
+            )}
+          </S.InputWrapper>
+        ))}
+      </S.InputsWrapper>
+      <Chip onClick={() => append('')}>추가</Chip>
+    </S.Wrapper>
+  )
+}
+
+export default MultiDoubleInput
