1cf5c7b321fb9c137ab1d0cf9fc8be6a5a6481c9
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-05-28 19:36:16 +0900

    feat: add seo component

--- packages/app/src/lib/env.ts
+++ packages/app/src/lib/env.ts
@@ -2,7 +2,8 @@ if (
   !(
     process.env.NEXT_PUBLIC_GAUTH_REDIRECT_URI &&
     process.env.NEXT_PUBLIC_GAUTH_CLIENT_ID &&
-    process.env.NEXT_PUBLIC_SERVER_URL
+    process.env.NEXT_PUBLIC_SERVER_URL &&
+    process.env.NEXT_PUBLIC_CLIENT_URL
   )
 )
   throw new Error('missing environment value')
@@ -11,6 +12,7 @@ const env = {
   NEXT_PUBLIC_GAUTH_REDIRECT_URI: process.env.NEXT_PUBLIC_GAUTH_REDIRECT_URI,
   NEXT_PUBLIC_GAUTH_CLIENT_ID: process.env.NEXT_PUBLIC_GAUTH_CLIENT_ID,
   NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  NEXT_PUBLIC_CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL,
 } as const
 
 export default env
