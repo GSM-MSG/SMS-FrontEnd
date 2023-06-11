4f3e3447a0105c5926461370722f9040592e2b79
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-06-12 00:02:10 +0900

    [#76] SEO 설정 추가 (#86)

--- packages/app/src/pages/login.tsx
+++ packages/app/src/pages/login.tsx
@@ -1,7 +1,13 @@
+import { SEO } from '@components'
 import LoginTemplate from '@templates/LoginTemplate'
 
 const Login = () => {
-  return <LoginTemplate />
+  return (
+    <>
+      <SEO title='login' />
+      <LoginTemplate />
+    </>
+  )
 }
 
 export default Login
