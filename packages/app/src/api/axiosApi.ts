8d93c596ab9305a59b2203d3063ebd139f9e496b
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-06-12 19:33:01 +0900

    [#91] 토큰 재발급 및 토큰 할당 로직 추가 (#92)

--- packages/app/src/api/axiosApi.ts
+++ packages/app/src/api/axiosApi.ts
@@ -1,6 +1,25 @@
 import axios from 'axios'
 import env from '@lib/env'
+import TokenManager from '@lib/TokenManager'
+import needsTokenRefresh from '@lib/needsTokenRefresh'
 
 const axiosApi = axios.create({ baseURL: env.NEXT_PUBLIC_SERVER_URL })
+
+axiosApi.interceptors.request.use(async (config) => {
+  const tokenManager = new TokenManager()
+
+  let isSuccessed = true
+
+  if (needsTokenRefresh(config.url, config.method))
+    isSuccessed = await tokenManager.reissueToken()
+
+  if (!isSuccessed) return { ...config, signal: AbortSignal.abort() }
+
+  config.headers['Authorization'] = tokenManager.accessToken
+    ? `Bearer ${tokenManager.accessToken}`
+    : undefined
+
+  return config
+})
 
 export default axiosApi
---
+++ packages/app/src/lib/Token.ts
@@ -0,0 +1,8 @@
+enum Token {
+  ACCESS_TOKEN = 'accessToken',
+  REFRESH_TOKEN = 'refreshToken',
+  ACCESS_TOKEN_EXP = 'accessTokenExp',
+  REFRESH_TOKEN_EXP = 'refreshTokenExp',
+}
+
+export default Token
