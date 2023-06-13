ffef5ae44eba29ddad3e3040af95307dc50d2dcb
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-06-14 00:08:55 +0900

    feat: add student detail modal responsive

--- packages/app/src/features/student/molecules/StudentDetail/index.tsx
+++ packages/app/src/features/student/molecules/StudentDetail/index.tsx
@@ -21,87 +21,93 @@ const StudentDetail = ({ student }: Props) => {
   return (
     <ModalPortal onClose={onClose}>
       <S.Wrapper>
-        {student.profileImg && (
-          <S.LeftImage src={student.profileImg} alt='profile image' />
-        )}
-        <S.RightContent>
-          <S.CloseButton onClick={onClose}>
-            <Xmark />
-          </S.CloseButton>
-          <S.Major>{student.major}</S.Major>
-          <S.Name>{student.name}</S.Name>
-          {student.grade &&
-            student.classNum &&
-            student.number &&
-            student.department && (
-              <S.SchoolInfo>
-                {student.grade}학년 {student.classNum}반 {student.number}번 •{' '}
-                {student.department}
-              </S.SchoolInfo>
-            )}
-          {student.techStacks.length && (
-            <S.Tags>
-              {student.techStacks.map((stack, idx) => (
-                <S.Tag key={idx}>{stack}</S.Tag>
-              ))}
-            </S.Tags>
+        <S.Content>
+          {student.profileImg && (
+            <S.LeftImage src={student.profileImg} alt='profile image' />
           )}
-          {student.introduce && (
-            <S.Introduce>
-              <S.IntroduceLabel>자기소개</S.IntroduceLabel>
-              {student.introduce}
-            </S.Introduce>
-          )}
-
-          {isExistDetailData && (
-            <S.RowTable>
-              <S.TableColumn>
-                <S.TableTitle>이메일</S.TableTitle>
-                <S.TableValue>{student.contactEmail}</S.TableValue>
-              </S.TableColumn>
-
-              <S.TableColumn>
-                <S.TableTitle>인증제점수</S.TableTitle>
-                <S.TableValue>{student.gsmAuthenticationScore}</S.TableValue>
-              </S.TableColumn>
-
-              <S.TableLine />
-
-              <S.TableColumn>
-                <S.TableTitle>병특 희망 여부</S.TableTitle>
-                <S.TableValue>{student.militaryService}</S.TableValue>
-              </S.TableColumn>
-
-              <S.TableColumn>
-                <S.TableTitle>근무 희망 지역</S.TableTitle>
-                <S.TableValue>{student.regions?.join(', ')}</S.TableValue>
-              </S.TableColumn>
-
-              <S.TableLine />
-
-              {student.languageCertificates?.map(
-                ({ languageCertificateName, score }, idx) => (
-                  <S.TableColumn key={idx}>
-                    <S.TableTitle>{languageCertificateName}</S.TableTitle>
-                    <S.TableValue>{score}</S.TableValue>
-                  </S.TableColumn>
-                )
+          <S.RightContent>
+            <S.CloseButton onClick={onClose}>
+              <Xmark />
+            </S.CloseButton>
+            <S.Major>{student.major}</S.Major>
+            <S.Name>{student.name}</S.Name>
+            {student.grade &&
+              student.classNum &&
+              student.number &&
+              student.department && (
+                <S.SchoolInfo>
+                  {student.grade}학년 {student.classNum}반 {student.number}번 •{' '}
+                  {student.department}
+                </S.SchoolInfo>
               )}
+            {student.techStacks.length && (
+              <S.Tags>
+                {student.techStacks.map((stack, idx) => (
+                  <S.Tag key={idx}>{stack}</S.Tag>
+                ))}
+              </S.Tags>
+            )}
+            {student.introduce && (
+              <S.Introduce>
+                <S.IntroduceLabel>자기소개</S.IntroduceLabel>
+                {student.introduce}
+              </S.Introduce>
+            )}
 
-              <S.TableLine />
+            {isExistDetailData && (
+              <S.RowTable>
+                <S.TableColumn>
+                  <S.TableTitle>이메일</S.TableTitle>
+                  <S.TableValue>{student.contactEmail}</S.TableValue>
+                </S.TableColumn>
 
-              <S.TableColumn>
-                <S.TableTitle>자격증</S.TableTitle>
-                <S.TableValue>{student.certificates?.join(', ')}</S.TableValue>
-              </S.TableColumn>
-            </S.RowTable>
-          )}
-          {student.portfolioUrl && (
-            <Button onClick={() => router.push(student.portfolioUrl as string)}>
-              포트폴리오
-            </Button>
-          )}
-        </S.RightContent>
+                <S.TableColumn>
+                  <S.TableTitle>인증제점수</S.TableTitle>
+                  <S.TableValue>{student.gsmAuthenticationScore}</S.TableValue>
+                </S.TableColumn>
+
+                <S.TableLine />
+
+                <S.TableColumn>
+                  <S.TableTitle>병특 희망 여부</S.TableTitle>
+                  <S.TableValue>{student.militaryService}</S.TableValue>
+                </S.TableColumn>
+
+                <S.TableColumn>
+                  <S.TableTitle>근무 희망 지역</S.TableTitle>
+                  <S.TableValue>{student.regions?.join(', ')}</S.TableValue>
+                </S.TableColumn>
+
+                <S.TableLine />
+
+                {student.languageCertificates?.map(
+                  ({ languageCertificateName, score }, idx) => (
+                    <S.TableColumn key={idx}>
+                      <S.TableTitle>{languageCertificateName}</S.TableTitle>
+                      <S.TableValue>{score}</S.TableValue>
+                    </S.TableColumn>
+                  )
+                )}
+
+                <S.TableLine />
+
+                <S.TableColumn>
+                  <S.TableTitle>자격증</S.TableTitle>
+                  <S.TableValue>
+                    {student.certificates?.join(', ')}
+                  </S.TableValue>
+                </S.TableColumn>
+              </S.RowTable>
+            )}
+            {student.portfolioUrl && (
+              <Button
+                onClick={() => router.push(student.portfolioUrl as string)}
+              >
+                포트폴리오
+              </Button>
+            )}
+          </S.RightContent>
+        </S.Content>
       </S.Wrapper>
     </ModalPortal>
   )
--- packages/app/src/features/student/molecules/StudentDetail/style.ts
+++ packages/app/src/features/student/molecules/StudentDetail/style.ts
@@ -1,26 +1,49 @@
 import styled from '@emotion/styled'
 
 export const Wrapper = styled.div`
+  width: 26.875rem;
+  height: 90vh;
+  position: relative;
+
+  @media (max-width: 30rem) {
+    width: 100%;
+    height: 100vh;
+    background: var(--WHITE);
+    z-index: -2;
+  }
+`
+
+export const Content = styled.div`
   display: grid;
-  width: 53.75rem;
-  height: 26.875rem;
-  grid-template-columns: 1fr 1fr;
+  width: 100%;
+  height: 100%;
+  grid-template-rows: 1fr 1fr;
   border: 0.5rem solid var(--N10);
   border-radius: 1.5rem;
-  overflow: hidden;
-  position: relative;
+  overflow: scroll;
+
+  @media (max-width: 30rem) {
+    border-radius: 0;
+    border: none;
+  }
 `
 
 export const LeftImage = styled.img`
   background: var(--WHITE);
   width: 100%;
   height: 100%;
+  position: sticky;
+  top: 0;
+  z-index: -1;
 `
 
 export const RightContent = styled.div`
   padding: 1.25rem;
-  overflow: scroll;
   background: var(--WHITE);
+
+  @media (max-width: 30rem) {
+    border-radius: 1.5rem;
+  }
 `
 
 export const CloseButton = styled.span`
@@ -63,7 +86,7 @@ export const Tag = styled.span`
   margin-bottom: 1rem;
 `
 
-export const Introduce = styled.p`
+export const Introduce = styled.div`
   ${({ theme }) => theme.body2}
   background: var(--N10);
   color: var(--BLACK);
@@ -72,7 +95,7 @@ export const Introduce = styled.p`
   margin-bottom: 1.5rem;
 `
 
-export const IntroduceLabel = styled.p`
+export const IntroduceLabel = styled.div`
   ${({ theme }) => theme.caption2}
   color: var(--N40);
   margin-bottom: 0.25rem;
