62ec1a84b161526badca3dfd215e2ffb1b39a8da
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-06-16 00:27:20 +0900

    feat: add page role

--- packages/app/src/features/auth/hook/useLoggedIn.tsx
+++ packages/app/src/features/auth/hook/useLoggedIn.tsx
@@ -1,7 +1,22 @@
 import loggedInApi from '@features/auth/service/loggedInApi'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const useLoggedIn = () => {
+  const router = useRouter()
   const { data, isSuccess } = loggedInApi.useLoggedInQuery()
+
+  useEffect(() => {
+    if (isSuccess && !data.isExist) router.push('/register')
+    if (!isSuccess && router.pathname === '/register') router.push('/login')
+    if (isSuccess && router.pathname === '/register' && data.isExist)
+      router.push('/')
+
+    if (isSuccess && router.pathname === '/login' && data.isExist)
+      router.push('/')
+    if (isSuccess && router.pathname === '/login' && !data.isExist)
+      router.push('/register')
+  }, [isSuccess])
 
   return {
     isExist: data?.isExist,
