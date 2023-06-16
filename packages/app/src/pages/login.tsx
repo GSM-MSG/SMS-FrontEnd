c635cd582cf49bf14b6521609b464fc02a2a9227
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-06-16 16:10:11 +0900

    [#111] 페이지별 권한 처리 (#119)

--- packages/app/src/pages/login.tsx
+++ packages/app/src/pages/login.tsx
@@ -1,7 +1,10 @@
 import { SEO } from '@components'
+import useLoggedIn from '@features/auth/hook/useLoggedIn'
 import LoginTemplate from '@templates/LoginTemplate'
 
 const Login = () => {
+  useLoggedIn()
+
   return (
     <>
       <SEO title='login' />
--- packages/app/src/pages/register.tsx
+++ packages/app/src/pages/register.tsx
@@ -1,7 +1,9 @@
+import useLoggedIn from '@features/auth/hook/useLoggedIn'
 import { RegisterForm } from '@features/register/organisms'
 import RegisterLayout from '@layouts/RegisterLayout'
 
 const Register = () => {
+  useLoggedIn()
   return (
     <RegisterLayout>
       <RegisterForm />
