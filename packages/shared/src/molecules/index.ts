18960026189e201ba41d23665fbdb7f391168d08
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-06-27 18:05:05 +0900

    fix: fix MutliRangeSlider style

--- packages/shared/src/molecules/index.ts
+++ packages/shared/src/molecules/index.ts
@@ -3,5 +3,6 @@ export { default as MultiDoubleInput } from './MultiDoubleInput'
 export { default as Select } from './Select'
 export { default as SearchInput } from './SearchInput'
 export { default as Header } from './Header'
+export { default as MultiRangeSlider } from './MultiRangeSlider'
 
 export type { OptionsType } from './Select/type'
