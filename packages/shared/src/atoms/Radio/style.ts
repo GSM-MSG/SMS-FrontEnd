188994ad877ecabe512cb40f52f6dabaa5f77d28
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-07-07 15:07:46 +0900

    feat: add radio label

--- packages/shared/src/atoms/Radio/style.ts
+++ packages/shared/src/atoms/Radio/style.ts
@@ -26,6 +26,14 @@ const beforeAnimation = keyframes`
 `
 
 export const Wrapper = styled.label`
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
+  color: var(--N30);
+`
+
+export const RadioButton = styled.label`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -41,8 +49,12 @@ export const Wrapper = styled.label`
 export const Input = styled.input`
   display: none;
 
-  :checked ~ #fake-checkbox {
+  :checked ~ span {
+    color: var(--N50);
+  }
+  :checked ~ label {
     border: 2px solid var(--P2);
+    color: var(--N50);
 
     ::after {
       display: block;
@@ -67,4 +79,8 @@ export const Input = styled.input`
       animation: ${beforeAnimation} 0.6s forwards;
     }
   }
+`
+
+export const Label = styled.span`
+  ${({ theme }) => theme.body2}
 `
