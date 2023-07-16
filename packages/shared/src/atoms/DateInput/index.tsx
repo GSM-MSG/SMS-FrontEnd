cf54de0bd7a39d8a7da4cead01f4661a3309cc71
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-07-16 14:29:20 +0900

    fix: fix DateInput

--- packages/shared/src/atoms/DateInput/index.tsx
+++ packages/shared/src/atoms/DateInput/index.tsx
@@ -1,13 +1,28 @@
-import { ChangeEvent } from 'react'
+import { ChangeEvent, DetailedHTMLProps, InputHTMLAttributes } from 'react'
 import * as S from './style'
 
-const DateInput = () => {
+interface Props
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {}
+
+const DateInput = ({ ...props }: Props) => {
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.value) e.target.classList.add('has-value')
     else e.target.classList.remove('has-value')
+
+    if (props.onChange) props.onChange(e)
   }
 
-  return <S.Input type='month' placeholder='yyyy.mm' onChange={onChange} />
+  return (
+    <S.Input
+      {...props}
+      type='month'
+      placeholder='yyyy.mm'
+      onChange={onChange}
+    />
+  )
 }
 
 export default DateInput
