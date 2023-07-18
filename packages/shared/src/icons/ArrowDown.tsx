4f2ec72690e1c7cf858dca972e2bf98a5e57e561
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-07-18 22:29:29 +0900

    fix: fix ArrowDown icon

--- packages/shared/src/icons/ArrowDown.tsx
+++ packages/shared/src/icons/ArrowDown.tsx
@@ -1,13 +1,13 @@
 const ArrowDown = () => (
   <svg
-    width='10'
-    height='6'
-    viewBox='0 0 10 6'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
   >
     <path
-      d='M1 1L5 5L9 1'
+      d='M6 9.5L12 15.5L18 9.5'
       stroke='black'
       strokeWidth='2'
       strokeLinecap='round'
