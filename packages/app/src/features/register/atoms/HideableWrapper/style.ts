99d108296c0d34e3b4b6c684fca056884b82878e
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-07-19 09:58:13 +0900

    feat: add HideableWrapper component

---
+++ packages/app/src/features/register/atoms/HideableWrapper/style.ts
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div``
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const Title = styled.h3`
+  ${({ theme }) => theme.title1}
+`
+
+export const Icons = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+`
+
+export const Icon = styled.label`
+  cursor: pointer;
+  height: 1.5rem;
+
+  svg {
+    transition: 0.2s;
+  }
+`
+
+export const HideAnimationInput = styled.input`
+  &:checked ~ svg {
+    transform: rotate(90deg);
+  }
+`
+
+interface ContentProps {
+  isChecked: boolean
+}
+
+export const Content = styled.div<ContentProps>`
+  margin-top: 2.5rem;
+  transition: 0.2s;
+
+  ${({ isChecked }) =>
+    isChecked &&
+    `
+    margin-top: 0;
+    height: 0;
+    overflow: hidden;
+  `}
+`
