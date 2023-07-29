58a4b6d961d84d6865edfe1180d82185b476b32e
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-07-29 23:22:38 +0900

    [#178] MonthPicker 추가 (#184)

---
+++ packages/shared/src/atoms/MonthPicker/index.tsx
@@ -0,0 +1,57 @@
+import {
+  DetailedHTMLProps,
+  forwardRef,
+  InputHTMLAttributes,
+  useState,
+} from 'react'
+import * as Icon from '../../icons'
+import Modal from './Modal'
+import * as S from './style'
+
+interface Props
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  value: string
+  setValue: (value: string) => void
+  clearError: () => void
+  error?: string
+}
+
+const MonthPicker = forwardRef<HTMLInputElement, Props>(
+  ({ value, setValue, error, clearError, ...props }, ref) => {
+    const [isShow, setIsShow] = useState<boolean>(false)
+
+    const onChange = (value: string) => {
+      setValue(value)
+      clearError()
+    }
+
+    return (
+      <S.Wrapper>
+        <input {...props} ref={ref} hidden />
+        <S.MonthInput
+          onClick={(e) => {
+            e.stopPropagation()
+            setIsShow(true)
+          }}
+        >
+          <S.Text>{value || 'yyyy.mm'}</S.Text>
+          <Icon.Calendar />
+          <Modal
+            isShow={isShow}
+            value={value}
+            onChange={onChange}
+            onClose={() => setIsShow(false)}
+          />
+        </S.MonthInput>
+        {error && <S.Error>{error}</S.Error>}
+      </S.Wrapper>
+    )
+  }
+)
+
+MonthPicker.displayName = 'MonthPicker'
+
+export default MonthPicker
