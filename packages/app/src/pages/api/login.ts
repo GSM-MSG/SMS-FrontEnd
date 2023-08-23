dd26c86a547331754d6db1f515e1e1be43be2846
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-08-23 16:08:34 +0900

    feat: add login BFF

---
+++ packages/app/src/pages/api/login.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { axiosApi } from '@api'
+import { TokenResponse } from '@features/auth/type/TokenResponse'
+import { isAxiosError } from 'axios'
+
+/**
+ * 로그인 BFF
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    return res.status(404).json({ message: '페이지가 존재하지 않습니다' })
+  }
+
+  const code = req.body.code
+  if (!code) {
+    return res.status(400).json({ message: '시크릿 값이 일치하지 않습니다' })
+  }
+
+  try {
+    const { data } = await axiosApi.post<TokenResponse>('/auth', { code })
+
+    res.setHeader('Set-Cookie', createSetCookie(data)).status(200).end()
+  } catch (e) {
+    if (!isAxiosError(e)) return res.status(500).end()
+
+    res.status(e.response?.status || 400).end()
+  }
+}
+
+const createSetCookie = (data: TokenResponse) => {
+  const accessToken = createCookie(
+    'accessToken',
+    data.accessToken,
+    data.accessTokenExp
+  )
+  const refreshToken = createCookie(
+    'refreshToken',
+    data.refreshToken,
+    data.refreshTokenExp
+  )
+
+  return `${accessToken} ${refreshToken}`
+}
+
+const createCookie = (name: string, token: string, expires: string) => {
+  const secure = process.env.NODE_ENV === 'production' ? 'secure;' : ''
+
+  return `${name}=${token}; path=/; expires=${expires}; httpOnly; ${secure}`
+}
