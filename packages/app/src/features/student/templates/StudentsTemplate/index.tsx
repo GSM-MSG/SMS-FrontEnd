073709194036e9254a545350a19707c7ca243f1a
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-08-31 15:24:31 +0900

    fix: fix filter modal SearchInput error

--- packages/app/src/features/student/templates/StudentsTemplate/index.tsx
+++ packages/app/src/features/student/templates/StudentsTemplate/index.tsx
@@ -1,12 +1,16 @@
 import StudentList from '@features/student/molecules/StudentList'
 import Header from '@features/global/Header'
+import { useModal } from '@features/modal/hooks'
+import FilterModal from '@features/student/molecules/FilterModal'
 
 import * as S from './style'
 
 const StudentsTemplate = () => {
+  const { onShow } = useModal()
+
   return (
     <S.Wrapper>
-      <Header />
+      <Header onFilter={() => onShow(<FilterModal />)} />
       <StudentList />
     </S.Wrapper>
   )
