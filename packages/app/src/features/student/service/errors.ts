1bc86eaaeb888d5efd45f78c5a76be0376bb376b
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-09-05 13:56:50 +0900

    fix: change url on errors

--- packages/app/src/features/student/service/errors.ts
+++ packages/app/src/features/student/service/errors.ts
@@ -2,7 +2,7 @@ import env from '@lib/env'
 import Errors from '@/types/Errors'
 
 const errors: Errors = {
-  [env.NEXT_PUBLIC_SERVER_URL]: {
+  [env.NEXT_PUBLIC_CLIENT_URL]: {
     '/student': {
       GET: {
         401: '페이지를 다시 로드해주세요',
