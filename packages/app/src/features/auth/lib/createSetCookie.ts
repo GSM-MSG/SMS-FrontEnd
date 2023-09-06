10d4d2117856e283a5342e1611925a185bc7bc9f
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-09-06 09:29:27 +0900

    [#250] 토큰 만료값 수정 (#253)

--- packages/app/src/features/auth/lib/createSetCookie.ts
+++ packages/app/src/features/auth/lib/createSetCookie.ts
@@ -19,8 +19,10 @@ export const createSetCookie = (data: TokenResponse) => {
 
 export const createCookie = (name: string, token: string, expired: string) => {
   const secure = process.env.NODE_ENV === 'production' ? 'secure;' : ''
-  const UTCDate = new Date(expired).getTime() - new Date().getTime()
+  const maxAge = (new Date(expired).getTime() - new Date().getTime()) / 1000
   const host = new URL(env.NEXT_PUBLIC_CLIENT_URL).host.replace(':3000', '')
 
-  return `${name}=${token}; path=/; max-age=${UTCDate}; httpOnly; ${secure} domain=${host}; SameSite=Strict`
+  return `${name}=${token}; path=/; max-age=${Math.floor(
+    maxAge
+  )}; httpOnly; ${secure} domain=${host}; SameSite=Strict`
 }
