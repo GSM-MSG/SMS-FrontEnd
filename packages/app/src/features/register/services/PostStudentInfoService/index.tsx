ea64d93207d4695cf4a5890101706e0871b79e28
이아론 <aron0613@naver.com>
2023-11-22 23:01:05 +0900

    feat: simplify registration workflow

--- packages/app/src/features/register/services/PostStudentInfoService/index.tsx
+++ packages/app/src/features/register/services/PostStudentInfoService/index.tsx
@@ -5,7 +5,6 @@ const PostStudentInfoService = async (form: RegisterFormType) => {
   try {
     await axiosApi.post('/server/student', {
       ...form,
-      prizes: form.prizes.map((i) => ({ ...i, kind: undefined, type: i.kind })),
     })
 
     return
--- packages/app/src/features/register/type/RegisterFormType.ts
+++ packages/app/src/features/register/type/RegisterFormType.ts
@@ -3,29 +3,7 @@ interface RegisterFormType {
   techStacks: string[]
   profileImgUrl: string
   introduce: string
-  stuNum: string
-  portfolioUrl: string
   contactEmail: string
-  formOfEmployment: FormOfEmployment // 희망 고용 형태
-  gsmAuthenticationScore: number // 인증제 점수
-  salary: number // 희망 연봉
-  regions: string[] // 근무지역
-  languageCertificates: LanguageCertificateType[]
-  militaryService: MilitaryService // 희망, 비희망, 상관없음, 해당없음
-  certificates: string[]
-  projects: ProjectType[]
-  prizes: PrizeType[]
-}
-
-interface PrizeType {
-  name: string
-  kind: string
-  date: string
-}
-
-interface LanguageCertificateType {
-  languageCertificateName: string
-  score: string
 }
 
 export default RegisterFormType
