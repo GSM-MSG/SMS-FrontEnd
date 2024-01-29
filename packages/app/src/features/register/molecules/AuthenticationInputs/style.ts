d96b203ac34a849aca60510861e1ba9ba6551f6d
송현우 <101445027+googoo81@users.noreply.github.com>
2024-01-29 18:41:26 +0900

    [#286] 학생 GSM 인증제 페이지 퍼블리싱 (#288)

---
+++ packages/app/src/features/register/molecules/AuthenticationInputs/style.ts
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled'
+
+export { Wrapper } from '@features/register/atoms/FormWrapper/style'
+
+export const ProjectList = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & div:last-of-type hr {
+    margin-bottom: 0;
+  }
+`
+
+export const ImageInput = styled.div`
+  width: 15.375rem;
+`
+
+export const Hr = styled.hr`
+  border: none;
+  border-top: 1px solid var(--N20);
+  margin: 2.5rem 0;
+`
+
+export const Title = styled.h3`
+  ${({ theme }) => theme.title1}
+`
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: end;
+`
+
+export const AddButton = styled.span`
+  ${({ theme }) => theme.title2}
+  display: flex;
+  align-items: center;
+  margin-top: 1rem;
+  cursor: pointer;
+`
