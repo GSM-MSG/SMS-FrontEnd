b9919f669dfb7e59b07ca69881e7152fc97113ab
happytaeyoon <s23021@gsm.hs.kr>
2024-06-02 15:31:58 +0900

    fix: props destructuring assignment

--- packages/shared/src/atoms/SegmentedControl/index.tsx
+++ packages/shared/src/atoms/SegmentedControl/index.tsx
@@ -16,22 +16,24 @@ interface Props
   text2: string
 }
 
-const SegmentedControl = forwardRef<HTMLInputElement, Props>((props, ref) => {
-  const id = useId()
-  return (
-    <S.Wrapper>
-      <S.Input hidden ref={ref} {...props} type='checkbox' id={id} />
-      <S.LabelWrapper htmlFor={id}>
-        <S.Label>{props.text1}</S.Label>
-        <S.Label>{props.text2}</S.Label>
-      </S.LabelWrapper>
-      <S.Slider className='Slider'>
-        <S.SliderText className='text1'>{props.text1}</S.SliderText>
-        <S.SliderText className='text2'>{props.text2}</S.SliderText>
-      </S.Slider>
-    </S.Wrapper>
-  )
-})
+const SegmentedControl = forwardRef<HTMLInputElement, Props>(
+  ({ text1, text2, ...props }, ref) => {
+    const id = useId()
+    return (
+      <S.Wrapper>
+        <S.Input hidden ref={ref} {...props} type='checkbox' id={id} />
+        <S.LabelWrapper htmlFor={id}>
+          <S.Label>{text1}</S.Label>
+          <S.Label>{text2}</S.Label>
+        </S.LabelWrapper>
+        <S.Slider className='Slider'>
+          <S.SliderText className='text1'>{text1}</S.SliderText>
+          <S.SliderText className='text2'>{text2}</S.SliderText>
+        </S.Slider>
+      </S.Wrapper>
+    )
+  }
+)
 
 SegmentedControl.displayName = 'SegmentedControl'
 
