fc3aa60b1bf733177ca8f5fd4223b3ab6afca8ce
변찬우 <byun00106@gmail.com>
2024-06-03 21:38:13 +0900

    fix: add comment on middleware

--- packages/app/src/middleware.ts
+++ packages/app/src/middleware.ts
@@ -3,6 +3,8 @@ import Token from '@lib/Token'
 import getRole from '@features/auth/lib/getRole'
 import Role from '@/types/Role'
 
+const notLoggedInRoute = ['/login']
+
 const protectedRoutes = [
   { path: '/mypage', roles: [Role.ROLE_STUDENT] },
   { path: '/register', roles: [Role.ROLE_STUDENT] },
@@ -12,20 +14,35 @@ const protectedRoutes = [
 
 export const middleware = (req: NextRequest) => {
   const { pathname } = req.nextUrl
+  const accessToken = req.cookies.get(Token.ACCESS_TOKEN)?.value
+  const url = req.nextUrl.clone()
 
+  // 로그인이 됐을 때 접근 불가
+  if (accessToken && notLoggedInRoute.includes(pathname)) {
+    url.pathname = '/'
+    return NextResponse.redirect(url)
+  }
+
+  // 보호되는 pathname인지 검사
   const route = protectedRoutes.find((route) => route.path === pathname)
   if (!route) return NextResponse.next()
 
-  const accessToken = req.cookies.get(Token.ACCESS_TOKEN)?.value
-  if (!accessToken) return NextResponse.redirect('/login')
-  const role = getRole(accessToken)
-  if (!role) return NextResponse.redirect('/login')
+  // 보호되는 pathname인데 accessToken이 없음
+  const role = getRole(accessToken || '')
+  if (!accessToken || !role) {
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
+  }
 
+  // 로그인은 됐는데 권한이 안 맞을 때
   if (!route.roles.includes(role)) {
-    const url = req.nextUrl.clone()
     url.pathname = '/'
     return NextResponse.redirect(url)
   }
 
   return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/mypage', '/register', '/register/teacher', '/gsminfo', '/login'],
 }
