8d87a0bce63c3bdbb14a71fdf126b6238d706179
정태관 <jxorhks86909763@gmail.com>
2024-06-04 17:25:08 +0900

    fix: scroll fixed function

--- packages/app/src/features/modal/portals/BlurPortal/index.tsx
+++ packages/app/src/features/modal/portals/BlurPortal/index.tsx
@@ -1,6 +1,6 @@
 import { usePortal } from '@features/modal/hooks'
 import ReactDOM from 'react-dom'
-import { cloneElement, ReactElement, useEffect } from 'react'
+import { cloneElement, ReactElement } from 'react'
 import * as S from './style'
 
 interface Props {
@@ -13,22 +13,6 @@ interface Props {
  */
 const BlurPortal = ({ children, isModalOpen }: Props) => {
   const portalValue = usePortal(true)
-
-  useEffect(() => {
-    if (isModalOpen) {
-      const scrollY = window.scrollY
-      document.body.style.cssText = `
-        position: fixed;
-        top: -${scrollY}px;
-        width: 100%;
-      `
-
-      return () => {
-        document.body.style.cssText = ''
-        window.scrollTo(0, scrollY)
-      }
-    }
-  }, [isModalOpen])
 
   if (!portalValue) return null
 
