ea080c54080167da3aae179b75286970ee7d3915
mini-aron <aron0613@naver.com>
2024-06-10 00:44:06 +0900

    feat: add type exception

--- packages/app/src/features/student/queries/useShareStudentDetailQuery.tsx
+++ packages/app/src/features/student/queries/useShareStudentDetailQuery.tsx
@@ -1,11 +1,17 @@
 import { useQuery } from '@tanstack/react-query'
 import ShareStudentDetailApi from '@features/student/service/ShareStudentDetailApi'
+import { useRouter } from 'next/router'
 
 interface Props {
   token: string
 }
 
 const useShareStudentDetailQuery = ({ token }: Props) => {
+  const router = useRouter()
+  if (token.length > 0) {
+    router.push('/404')
+  }
+
   return useQuery<StudentDetail>({
     queryKey: [token],
     queryFn: () => ShareStudentDetailApi(token),
