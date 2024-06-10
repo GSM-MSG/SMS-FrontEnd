cd08d4c89fa943fff3d80a6a57fcf0dcb2513026
mini-aron <aron0613@naver.com>
2024-06-10 20:01:16 +0900

    fix: chage Student detail prop type

--- packages/app/src/features/student/molecules/StudentDetail/index.tsx
+++ packages/app/src/features/student/molecules/StudentDetail/index.tsx
@@ -5,7 +5,7 @@ import StudentInfo from '@features/student/atoms/StudentInfo'
 import * as S from './style'
 
 interface Props {
-  student: StudentDetail
+  student: StudentDetail | null
 }
 
 const StudentDetail = ({ student }: Props) => {
