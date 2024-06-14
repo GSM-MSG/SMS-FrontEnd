f86d44309c3cb513c3b8e8de97c5bb2f0f449580
변찬우 <byun00106@gmail.com>
2024-06-15 00:03:23 +0900

    fix: resolve retry problem on link page

--- packages/app/src/features/student/service/ShareStudentDetailApi.ts
+++ packages/app/src/features/student/service/ShareStudentDetailApi.ts
@@ -2,7 +2,7 @@ import { axiosApi } from '@api'
 
 const ShareStudentDetailApi = async (token: string): Promise<StudentDetail> => {
   const { data } = await axiosApi.get<StudentDetail>(
-    `/server/student/link?token=${token}`
+    `/api/server/student/link?token=${token}`
   )
   return data
 }
