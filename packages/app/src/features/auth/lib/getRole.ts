b0397301e219c15a79d10354b8c90b07913e7a9f
변찬우 <byun00106@gmail.com>
2024-06-16 04:36:08 +0900

    feat: add teacher role

--- packages/app/src/features/auth/lib/getRole.ts
+++ packages/app/src/features/auth/lib/getRole.ts
@@ -2,7 +2,14 @@ import { decode } from 'jsonwebtoken'
 import { z } from 'zod'
 import Role from '@/types/Role'
 
-const RoleSchema = z.enum([Role.ROLE_STUDENT, Role.ROLE_TEACHER])
+const RoleSchema = z.enum([
+  Role.ROLE_STUDENT,
+  Role.ROLE_TEACHER,
+  Role.ROLE_DIRECTOR,
+  Role.ROLE_HOMEROOM,
+  Role.ROLE_PRINCIPAL,
+  Role.ROLE_DEPUTY_PRINCIPAL,
+])
 
 const getRole = (token: string) => {
   const payload = decode(token, { json: true })
--- packages/app/src/middleware.ts
+++ packages/app/src/middleware.ts
@@ -16,6 +16,13 @@ export const middleware = (req: NextRequest) => {
   const { pathname } = req.nextUrl
   const accessToken = req.cookies.get(Token.ACCESS_TOKEN)?.value
   const url = req.nextUrl.clone()
+  const role = getRole(accessToken || '')
+
+  // 회원가입을 하지 않은 선생님
+  if (role === Role.ROLE_TEACHER && pathname !== '/register/teacher') {
+    url.pathname = '/register/teacher'
+    return NextResponse.redirect(url)
+  }
 
   // 로그인이 됐을 때 접근 불가
   if (accessToken && notLoggedInRoute.includes(pathname)) {
@@ -28,7 +35,6 @@ export const middleware = (req: NextRequest) => {
   if (!route) return NextResponse.next()
 
   // 보호되는 pathname인데 accessToken이 없음
-  const role = getRole(accessToken || '')
   if (!accessToken || !role) {
     url.pathname = '/login'
     return NextResponse.redirect(url)
