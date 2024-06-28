97ab3af1a35dc6f150cea08abf26d4db48a4d778
변찬우 <byun00106@gmail.com>
2024-06-29 00:36:36 +0900

    build: resolved build error

---
+++ packages/app/src/features/register/atoms/HideAbleWrapper/index.tsx
@@ -0,0 +1,46 @@
+import { ReactNode, useState } from 'react'
+import * as Icon from '@sms/shared/src/icons'
+import { useRouter } from 'next/router'
+import * as S from './style'
+
+interface Props {
+  onDelete: () => void
+  children: ReactNode
+  title: string
+}
+
+const HideAbleWrapper = ({ children, title, onDelete }: Props) => {
+  const router = useRouter()
+  const [isChecked, setIsChecked] = useState<boolean>(false)
+
+  return (
+    <>
+      <S.Header>
+        <S.Title>
+          {title ||
+            (router.pathname === '/mypage'
+              ? '프로젝트 제목이 없습니다'
+              : '활동 제목이 없습니다')}
+        </S.Title>
+        <S.Icons>
+          <S.Icon>
+            <S.HideAnimationInput
+              onClick={() => setIsChecked(!isChecked)}
+              hidden
+              type='checkbox'
+            />
+            <Icon.ArrowDown />
+          </S.Icon>
+
+          <S.Icon onClick={onDelete}>
+            <Icon.XMarkOutline />
+          </S.Icon>
+        </S.Icons>
+      </S.Header>
+
+      <S.Content isChecked={isChecked}>{children}</S.Content>
+    </>
+  )
+}
+
+export default HideAbleWrapper
