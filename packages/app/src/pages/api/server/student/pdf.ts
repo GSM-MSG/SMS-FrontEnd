ccdd37fe7ecb975eafd31844060bf2256a97630e
이아론 <aron0613@naver.com>
2024-07-20 21:31:43 +0900

    fix: delete handler

--- packages/app/src/pages/api/server/student/pdf.ts
+++ packages/app/src/pages/api/server/student/pdf.ts
@@ -1,5 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
 export const config = {
   api: {
     bodyParser: {
@@ -7,13 +5,3 @@ export const config = {
     },
   },
 }
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'PUT') {
-    res.status(200).json({ message: 'File uploaded successfully' })
-  } else {
-    res.status(413).json({ message: 'Method not allowed' })
-  }
-}
-
-export default handler
