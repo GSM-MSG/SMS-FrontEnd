406d1512a0b3c57dd1cd8b4a0dea6bea49a670b6
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2024-08-15 14:37:31 +0900

    [#357] 학생 리스트 페이지 버그 수정 (#358)

--- packages/app/src/features/student/queries/useStudentDetailQuery.tsx
+++ packages/app/src/features/student/queries/useStudentDetailQuery.tsx
@@ -7,9 +7,10 @@ interface Props {
 
 const useStudentDetailQuery = ({ studentId }: Props) => {
   return useQuery({
-    queryKey: ['studentDetail'],
+    queryKey: ['studentDetail', studentId],
     queryFn: () => studentDetailApi(studentId as string),
     enabled: typeof studentId === 'string',
+    placeholderData: (data) => data,
   })
 }
 
